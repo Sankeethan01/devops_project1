0d19703a2bfe7b136079fca3aeb4388aa71d1de9
Sankeethan01 <sankee26july@gmail.com>
2025-03-28 18:55:27 +0530

    app.js changed: display message modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello this is the beginning of the project!');
+  res.send('Hello this is the beginning of the project!, now we are going to deploy it to Heroku');
 });
 
 app.listen(port, () => {
